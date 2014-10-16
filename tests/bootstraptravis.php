39e98c5115e11bd2e9f585af0143b10af775341a
ArthurHlt <arthurh.halet@gmail.com>
2014-10-16 21:47:38 +0200

    - fix for hhvm test + add status travis in readme

--- tests/bootstraptravis.php
+++ tests/bootstraptravis.php
@@ -1,6 +1,7 @@
 <?php
 ini_set("variables_order", "EGPCS");
 error_reporting(E_ALL & ~E_NOTICE);
+date_default_timezone_set('GMT');
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 $loader->add('Arthurh\\Sphring\\', __DIR__);
 $logger = new \Monolog\Logger("SphringTest");
